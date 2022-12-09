bd19e3b93a7a5ffefd3b89f7ad5dad0321858b6a
Anastasiya Kochkina <kochkinaanastasiya@yandex.ru>
2022-12-09 23:51:09 +0300

    first commit

---
+++ src/main/java/com/kochkina/homework2_15/Homework215Application.java
@@ -0,0 +1,13 @@
+package com.kochkina.homework2_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework215Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework215Application.class, args);
+	}
+
+}
